bcd3a0ec213c9e3173b0d5de6419f8b9ac61d8b5
reactnetau <polyrhythmm@gmail.com>
2023-04-13 12:32:02 +1000

    change image height for flatlist item

--- src/components/MovieItem/MovieItem.tsx
+++ src/components/MovieItem/MovieItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Text, Image, Pressable, StyleProp, ViewStyle, TextProps } from 'react-native'
+import { Text, Image, Pressable, StyleProp, ViewStyle, TextProps, ImageStyle } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
-import { CreateNavigationProp, IMovie, IMovieProps } from '../../types/interfaces';
+import { CreateNavigationProp, IMovie, } from '../../types/interfaces';
 import styles from './MovieItem.scss';
 
 const MovieItem = ({ movie } :IMovie ) => {
@@ -10,7 +10,7 @@ const MovieItem = ({ movie } :IMovie ) => {
 
     return (
         <Pressable style={styles.item as StyleProp<ViewStyle>} onPress={() => navigation.navigate("Movie", { id, name: movie.Title })}>
-            <Image source={{ uri: movie.Poster }} style={{width: '100%', aspectRatio: 1}} />
+            <Image source={{ uri: movie.Poster }} style={styles.image as StyleProp<ImageStyle>} />
             <Text style={styles.text as Readonly<TextProps>}>{movie.Title}</Text>
             <Text style={styles.text as Readonly<TextProps>}>{movie.Year}</Text>
         </Pressable>
